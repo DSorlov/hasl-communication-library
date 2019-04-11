dc9a69c9bd1d8e84d0fea72e7c84c1fa18d59d6d
Daniel Sörlöv <dsorlov@users.noreply.github.com>
2019-04-11 22:45:16 +0200

    Update hasl.py

--- hasl/hasl.py
+++ hasl/hasl.py
@@ -9,7 +9,7 @@ from .exceptions import *
 from .version import __version__
 
 _BASE_URL = 'https://api.sl.se/api2/'
-_DEPARTURE_URL = _BASE_URL + 'realtimedeparturesV4.json?key={}&siteid={}'
+_DEPARTURE_URL = _BASE_URL + 'realtimedeparturesV4.json?key={}&siteid={}&timeWindow={}'
 _DEVIATION_URL = _BASE_URL + 'deviations.json?key={}&siteid={}&lineNumber={}'
 _USER_AGENT = "HASL/"+__version__
 _AUTH_ERRS = (401, 403)
@@ -19,18 +19,20 @@ _LOGGER = logging.getLogger(__name__)
 
 class hasl(object):
 
-    def __init__(self, api_token, siteid, lines, timeout=None):
-        self._api_token: api_token,
+    def __init__(self, deviation_api_token, departure_api_token, siteid, lines, window, timeout=None):
+        self._deviation_api_token: deviation_api_token,
+		self._departure_api_token: departure_api_token,
         self._siteid: api_token,
         self._lines: lines,
-        self._timeout = timeout
+		self._window: window,
+        self._timeout = timeout	
 
     def _get(self, url):
 	
 		resp = requests.get(url, headers={"User-agent": USER_AGENT}, allow_redirects=True, timeout=self._timeout,headers={'User-agent': _USER_AGENT})
 	
 		if resp.status_code in (401, 403):
-         _LOGGER.error("hasl: Failed fetching data for '%s'"
+         _LOGGER.error("HASL: Failed fetching data for '%s'"
                           "(HTTP Status_code = %d)", url,
                           resp.status_code) 
 
@@ -38,7 +40,7 @@ class hasl(object):
         return resp.json()
 
     def get_departures(self):
-        return self._get(_DEPARTURE_URL.format(self._api_token,self._siteid))
+        return self._get(_DEPARTURE_URL.format(self._api_token,self._siteid,self._window))
 
     def get_deviations(self):
         return self._get(_DEVIATION_URL.format(self._api_token,self._siteid,self._lines))
